29b1976aee39b3c47e6d4fe84433398d736e501f
jhonaquedajau <jhona.quejada@gmail.com>
2022-03-21 23:19:46 -0500

    Actualizacion backend

--- source/backend/src/backend.py
+++ source/backend/src/backend.py
@@ -4,7 +4,7 @@ sys.path.append("../")
 sys.path.append("../../")
 
 import grpc
-
+import os.path
 import backend_pb2_grpc
 import backend_pb2
 
@@ -16,19 +16,96 @@ class Backend(backend_pb2_grpc.BackendServicer):
 
 
     def load_image(self, request, context):
-        patient = request.name 
-        pass
+        image_path = request.image_path 
+
+        if os.path.isfile(image_path):
+            print ("La ruta de la imagen es valida")
+            return image_path
+        else:
+            print ("Por favor cargue un archivo valido")
+        
 
     def get_prediction(self, request, context):
-        pass
+        pred_image = request.pred_image
+    
+        if os.path.exists(pred_image):
+            print ("La predicción está lista")
+            return pred_image
+        else:
+            print ("Esperando por la prediccion en inferencia")
 
     def get_db_status(self, request, context):
-        pass
+        
+        # Obtenemos los datos del paciente
+        image_path = request.image_path
+        pred_image = request.pred_image
+        name = request.name
+        last_name = request.last_name
+        id_type = request.id_type
+        id_num = request.id_num
+        gender = request.gender
+                
+        if os.path.isfile(image_path):
+            print("Imagen cargada con exito")
+            return image_path
+        else:
+            print("La imagen no es valida")
+        
+        if os.path.isfile(image_path):
+            print("Predicción cargada con exito")
+            return image_path
+        else:
+            print("La prediccion no puede ser hecha")
+
+        if name and last_name and gender is str():
+            print("Archivos cargados con exito")
+            return name, last_name, gender
+        else:
+            print("Los archivos no son de tipo texto")
+        
+        if id_num and id_type is int():
+            print("Archivos cargados con exito")
+            return id_num, id_type
+        else:
+            print("Los archivos no son de tipo numerico")
+
 
     def save_data(self, request, context):
-        pass
+        # Obtenemos los datos del paciente
+        image_path = request.image_path
+        pred_image = request.pred_image
+        name = request.name
+        last_name = request.last_name
+        id_type = request.id_type
+        id_num = request.id_num
+        gender = request.gender
+                
+        if os.path.isfile(image_path):
+            print("Imagen cargada con exito")
+            return image_path
+        else:
+            print("La imagen no es valida")
+        
+        if os.path.isfile(image_path):
+            print("Predicción cargada con exito")
+            return image_path
+        else:
+            print("La prediccion no puede ser hecha")
+
+        if name and last_name and gender is str():
+            print("Archivos cargados con exito")
+            return name, last_name, gender
+        else:
+            print("Los archivos no son de tipo texto")
+        
+        if id_num and id_type is int():
+            print("Archivos cargados con exito")
+            return id_num, id_type
+        else:
+            print("Los archivos no son de tipo numerico")
 
     def create_pdf(self, request, context):
+
         pass
 
 
