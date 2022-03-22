de4ca7b2e2762e52ea4aee3b0edc074e7d83367e
DianaDelgado <lunita.sud@gmail.com>
2022-03-21 19:29:00 -0500

    mostrar imagen en pantalla

--- source/ui/src/control_ui.py
+++ source/ui/src/control_ui.py
@@ -1,20 +1,19 @@
+from base64 import b64decode
+import io
+import cv2 as rq
+#import backend_pb2_grpc as grpc
+#import backend_pb2 as cli
 
-
-
-def callInference():
+def callInference(attr, old, new):
+    image = io.BytesIO(b64decode(new))
     #llamar el servicio de load_image del backend y mandarle el ui_data
     #channel = grpc.insecure_channel('localhost:50051')
     #stub = cli.BackendStub(channel)
-    #response = stub.load_image(rq.ui_data( ui.txiNombres.value,ui.txiApellidos.value, ui.txiTipo.value, ui.txiNumDoc.value))
-    #image_inference = stub.load_image(rq.ui_data(name, lastname,tipoId, numId,gender, pathIm))
-    print("Nombre: ")
-    
-def callInference2(image_path):
-    print("Nombre: "+image_path)
+    #image_inference = stub.load_image(rq.ui_data(name, lastname,tipoId, numId,gender, image))
+    print("Nombre: "+image)
 
-def callPdf():
-    
-    pass
+def callPdf(attr, old, new):
+    print("callPDF:"+new)
     
     
 
